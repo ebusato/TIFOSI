fbe7fbc376fe3ed849ab1d4e6cd4a746f2542484
Busato <emmanuel.busato@gmail.com>
2018-05-23 09:15:35 +0200

    add files

---
+++ run/run.sh
@@ -0,0 +1,7 @@
+ root -l 'runBayesianMCMC.C("../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2/CH.All.dat","../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2_METdown_100/CH.All.dat","../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2_METup_100/CH.All.dat","../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_3/CH.All.dat","../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_3_METup_40/CH.All.dat")'
+
+#root -l 'runBayesianMCMC.C("../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2/CH.All.dat")'
+
+#root -l 'runBayesianMCMC.C("../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2_METdown_100/CH.All.dat")'
+
+#root -l 'runBayesianMCMC.C("../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2/CH.All.dat","../inputs/Files4topContactInt_11dec2014/Ht+Lt_700_NbbJets_2_METdown_100/CH.All.dat")'
---
+++ setup.sh
@@ -0,0 +1,5 @@
+if [[ -n "${LD_LIBRARY_PATH+1}" ]]; then
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ebusato/Travail/TIFOSI
+else
+  export LD_LIBRARY_PATH=/home/ebusato/Travail/TIFOSI
+fi
